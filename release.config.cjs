083133dd1ce1baecd68d06414a3a498334930cc0
LStivan <l.stivan@advocaciabichara.com.br>
2025-09-30 15:30:09 -0300

    final main

--- release.config.cjs
+++ release.config.cjs
@@ -1,9 +1,26 @@
+// release.config.cjs
 module.exports = {
   branches: ["main"],
   plugins: [
+    // Analisa commits para decidir o tipo de versão
     ["@semantic-release/commit-analyzer", { preset: "angular" }],
+
+    // Gera notas de release com base nos commits
     ["@semantic-release/release-notes-generator", { preset: "angular" }],
+
+    // Atualiza o changelog
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+
+    // Atualiza package.json com a nova versão
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: ".", // diretório onde está o package.json
+        npmPublish: false, // não publica no npm
+      },
+    ],
+
+    // Commita package.json, pnpm-lock.yaml e CHANGELOG.md
     [
       "@semantic-release/git",
       {
