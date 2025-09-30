ee61b88ac2b3b0539ee903032e9ff94d2e8500d4
LStivan <l.stivan@advocaciabichara.com.br>
2025-09-30 14:45:46 -0300

    chore: ajuste biome e realise

--- release.config.js
+++ release.config.js
@@ -1,29 +1,18 @@
-module.exports = {
-  branches: ["main"], // só executa na main
+// release.config.js
+export default {
+  branches: ["main"],
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        preset: "angular", // usa Conventional Commits padrão
-      },
-    ],
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        preset: "angular",
-      },
-    ],
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
+    ["@semantic-release/commit-analyzer", { preset: "angular" }],
+    ["@semantic-release/release-notes-generator", { preset: "angular" }],
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
-        message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
+        assets: ["package.json", "pnpm-lock.yaml", "CHANGELOG.md"],
+
+        message:
+          // biome-ignore lint/suspicious/noTemplateCurlyInString: <>
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
